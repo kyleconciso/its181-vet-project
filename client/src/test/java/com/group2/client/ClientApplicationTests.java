ee1f3c92e9d9ff3f917a93ed7cf508dbbe5a2cfe
kyleconciso <kyleconciso@gmail.com>
2025-03-17 12:19:20 +0800

    add Spring initializr project

---
+++ client/src/test/java/com/group2/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
